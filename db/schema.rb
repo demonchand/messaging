e2fccadfac6f313ec5220b2a0bddc02996f0f9a5
chandru <chandru@xtrafin.net>
2011-04-01 13:05:25 +0530

    some changes are made

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,18 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110401054531) do
+ActiveRecord::Schema.define(:version => 20110401072710) do
+
+  create_table "messages", :force => true do |t|
+    t.string   "from"
+    t.string   "to"
+    t.string   "subject"
+    t.text     "body"
+    t.datetime "date"
+    t.string   "status"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", :force => true do |t|
     t.string   "email",                               :default => "", :null => false
