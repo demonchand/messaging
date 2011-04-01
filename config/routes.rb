e9c95107d92e3765767879b048549bf6fadc4142
chandru <chandru@xtrafin.net>
2011-04-01 11:38:22 +0530

    some basic modifications are done

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Messaging::Application.routes.draw do
 
   devise_for :users
 
+  root :to => "home#index"
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -58,5 +59,5 @@ Messaging::Application.routes.draw do
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
-  # match ':controller(/:action(/:id(.:format)))'
+  match ':controller(/:action(/:id(.:format)))'
 end
