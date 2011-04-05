2fc2ab871c452d91c6669b30ce722da24f0ead23
chandru <chandru@xtrafin.net>
2011-04-05 17:22:31 +0530

    now the double entry is working

--- app/models/journal.rb
+++ app/models/journal.rb
@@ -4,10 +4,32 @@ class Journal < ActiveRecord::Base
   belongs_to :account
   
   def edit_account_type_balance
-    if self.from = "assert"
-      aa = Account.find_by_account_type(self.from.to_s)
-      aa.balance = (aa.balance - self.amount)
-      aa.save!
+  #  case self.from
+  #  when "1" then
+  #    name = "assert"
+  #    credit(name)
+  #    name = 
+  #  end
+    if self.from == "1"
+      credit
+      debit
+      cred.save!
     end
+  end
+  
+  def credit
+    cred = Account.find_by_account_type("assert")
+    cred.balance = (cred.balance - self.amount)
+  end
+  
+  def debit
+    if self.to == "2"
+      deb = Account.find_by_account_type("expense")
+      deb.balance = (deb.balance + self.amount)
+      deb.save!
+    end
+  end
+  def get_the_string
+    @name1 = Account.find(1).account_type
   end
 end
