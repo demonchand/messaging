3e5e8ce2a5e8c16132ce53b76af8400f187581f9
chandru <chandru@xtrafin.net>
2011-04-05 15:49:23 +0530

    after save is done and relationship also

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,5 +1,6 @@
 class Account < ActiveRecord::Base
-
+  has_many :journals
+  
   def to_s
     "#{account_type}"
   end
--- app/models/journal.rb
+++ app/models/journal.rb
@@ -1,2 +1,13 @@
 class Journal < ActiveRecord::Base
+  after_create :edit_account_type_balance
+  
+  belongs_to :account
+  
+  def edit_account_type_balance
+    if self.from = "assert"
+      aa = Account.find_by_account_type(self.from.to_s)
+      aa.balance = (aa.balance - self.amount)
+      aa.save!
+    end
+  end
 end
