ccc2e8469977731d34807908ab97e5b8cab30c03
chandru <chandru@xtrafin.net>
2011-04-06 11:00:06 +0530

    find a new way to create a credit and debit

--- app/models/journal.rb
+++ app/models/journal.rb
@@ -4,32 +4,47 @@ class Journal < ActiveRecord::Base
   belongs_to :account
   
   def edit_account_type_balance
-  #  case self.from
-  #  when "1" then
-  #    name = "assert"
-  #    credit(name)
-  #    name = 
-  #  end
-    if self.from == "1"
-      credit
-      debit
-      cred.save!
-    end
+#######    if self.from == "1"
+#######      credit
+#######      debit
+#######      cred.save!
+#######    end
+#######  end
+#######  
+#######  def credit
+#######    cred = Account.find_by_account_type("assert")
+#######    cred.balance = (cred.balance - self.amount)
+#######  end
+#######  
+#######  def debit
+#######    if self.to == "2"
+#######      deb = Account.find_by_account_type("expense")
+#######      deb.balance = (deb.balance + self.amount)
+#######      deb.save!
+    #######    end
+
+    credit(self.from)
+    debit(self.to)
   end
-  
-  def credit
-    cred = Account.find_by_account_type("assert")
-    cred.balance = (cred.balance - self.amount)
+
+
+  ## This is my another way of working style Above is the old and that
+    ## is working
+    
+#    get_the_string_and_value(self.from, self.to)
+
+  def credit(val1)
+    cred = Account.find(val1.to_i)
+    cred.update_attributes(:balance => cred.balance - self.amount)
+    cred.save!
   end
-  
-  def debit
-    if self.to == "2"
-      deb = Account.find_by_account_type("expense")
-      deb.balance = (deb.balance + self.amount)
-      deb.save!
-    end
+  def debit(val2)
+    debt = Account.find(val2.to_i)
+    debt.update_attributes(:balance => debt.balance + self.amount)
   end
-  def get_the_string
-    @name1 = Account.find(1).account_type
+  def get_the_string_and_value(val1, val2)
+    @credit_bal = Account.find(val1.to_i).balance
+    @debit_bal = Account.find(val2.to_i).balance
   end
 end
+
